1addb3fec567318fa3a0a371192043ca17326f91
luisfmaiadc <lf.mcosta23@gmail.com>
2024-10-14 14:35:37 -0300

    Criação do Projeto de API REST de Catálogo de Filmes com Spring Initializr com todas as dependências necessárias para o projeto.

---
+++ src/main/java/com/portfolio/api_catalogo_filmes/ApiCatalogoFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_catalogo_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalogoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCatalogoFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/api_catalogo_filmes/ApiCatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_catalogo_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
